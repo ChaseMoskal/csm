137982e237cab7400d6e6ce0956dc0415ececd4f
Chase Moskal <chasemoskal@gmail.com>
2017-01-10 21:56:49 -0800

    Synthesizer and midi listener refactor
     - Synthesizer now uses Tone JS PolySynth to allow simultaneous notes
       (chords).
    
     - Update midi-listener to 0.2.0.
    
     - Rearrange the npm build script names.

--- source/Synth.ts
+++ source/Synth.ts
@@ -1,27 +1,56 @@
 
-import {NoteReport} from "midi-listener"
 import * as Tone from "tone"
+import MidiListener, {NoteReport} from "midi-listener"
 
 /**
- * Super simplistic synth, to verify that MIDI input is working.
+ * Options for instruments.
+ */
+export interface InstrumentOptions {
+
+  /** Midi listener which reports notes for the instrument to play. */
+  midiListener: MidiListener
+}
+
+/**
+ * Synthesizer which plays notes in accordance to the provided midi listener.
  */
 export default class Synth {
 
-  private readonly instrument = new Tone.Synth({
-    oscillator: {
-      type: "pwm",
-      modulationFrequency: 0.2
-    },
-    envelope: {
-      attack: 0.02,
-      decay: 0.1,
-      sustain: 0.2,
-      release: 0.9
-    }
-  }).toMaster()
+  /** Tone JS instrument. */
+  private instrument
 
-  play({code, velocity, frequency}: NoteReport) {
-    if (velocity > 0) this.instrument.triggerAttack(frequency, undefined, velocity * 2);
-    else this.instrument.triggerRelease()
+  /**
+   * Create a synthesizer.
+   */
+  constructor({midiListener}: InstrumentOptions) {
+
+    // Play notes from the midi listener.
+    midiListener.subscribe({onNote: report => this.play(report)})
+
+    // Initialize the Tone JS instrument.
+    this.instrument = new Tone.PolySynth(6, Tone.Synth)
+    this.instrument.set({
+      oscillator: {
+        type: "pwm",
+        modulationFrequency: 0.4
+      },
+      envelope: {
+        attack: 0.02,
+        decay: 0.1,
+        sustain: 0.2,
+        release: 0.5
+      }
+    })
+    this.instrument.toMaster()
+  }
+
+  /**
+   * Play the provided note report.
+   */
+  private play({code, velocity, frequency}: NoteReport) {
+    if (velocity > 0)
+      this.instrument.triggerAttack(frequency, undefined, velocity * 2)
+    else
+      this.instrument.triggerRelease(frequency)
   }
 }
